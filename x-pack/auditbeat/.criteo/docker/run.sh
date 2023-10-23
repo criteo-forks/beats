832b8f884dc4a461f095af3f55130965ad63d66f
jmbass <jmp9412@gmail.com>
2023-10-23 13:37:34 +0000

    ci: add criteo install script

--- x-pack/auditbeat/.criteo/docker/run.sh
+++ x-pack/auditbeat/.criteo/docker/run.sh
@@ -2,6 +2,6 @@
 
 docker build -t auditbeat-build -f ./x-pack/auditbeat/.criteo/docker/Dockerfile .
 cid=`docker create auditbeat-build`
-docker cp $cid:/home/ci/beats/x-pack/auditbeat/auditbeat .
+docker cp $cid:/home/ci/beats/x-pack/auditbeat/auditbeat auditbeat-criteo
 docker cp $cid:/home/ci/beats/x-pack/auditbeat/auditbeat.yml .
 docker rm $cid
